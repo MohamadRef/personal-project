f3ba90d1d1b6978d88d295c00cadef8ac37e75f8
MohamadRef <mhr2@sfu.ca>
2025-02-13 22:51:50 -0800

    next config changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: 'export',
+  basePath: '/repo-name',
+  assetPrefix: '/repo-name/',
+  images: {
+    unoptimized: true, // Disable Next.js image optimization for static exports
+  },
 };
 
 export default nextConfig;
